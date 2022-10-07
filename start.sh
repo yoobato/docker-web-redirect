00ab3d1b6b63aae743f2f0070a8fa966a3f5d035
yoobato <yoobato@gmail.com>
2022-10-06 22:59:33 -0700

    Use redirect instead of rewrite

--- start.sh
+++ start.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
-if [ -z "$REDIRECT_TYPE" ]; then
-	REDIRECT_TYPE="permanent"
+
+# Default to 302 (Temporary Redirect)
+if [ -z "${REDIRECT_CODE}" ]; then
+	REDIRECT_CODE="302"
 fi
 
-if [ -z "$REDIRECT_TARGET" ]; then
-	echo "Redirect target variable not set (REDIRECT_TARGET)"
+# Validate HTTP Redirect Code
+# 301 : Permanant / GET when request to redirected location
+# 308 : Permanant / Same HTTP method
+# 302 : Temporary / Originally same as 307, but most browsers only use GET
+# 303 : Temporary / GET
+# 307 : Temporary / Same HTTP method
+echo "301, 302, 303, 307, 308" | grep -qw ${REDIRECT_CODE}
+if [[ $? -ne 0 ]]; then
+	echo -e "Invalid REDIRECT_CODE(${REDIRECT_CODE}).\nUse 301, 308 for Permanant Redirects.\nUse 302, 303, 307 for Temporary Redirects."
+	exit 1
+fi
+
+# Check if REDIRECT_TARGET is set
+if [ -z "${REDIRECT_TARGET}" ]; then
+	echo "(REDIRECT_TARGET) variable not set"
 	exit 1
 else
-	# Add http if not set
-	if ! [[ $REDIRECT_TARGET =~ ^https?:// ]]; then
-		REDIRECT_TARGET="http://$REDIRECT_TARGET"
+	# Add http it not set
+	if ! [[ ${REDIRECT_TARGET} =~ ^https?:// ]]; then
+		REDIRECT_TARGET="http://${REDIRECT_TARGET}"
 	fi
 
-	# Add trailing slash
+	# Add trailing slash it not set
 	if [[ ${REDIRECT_TARGET:length-1:1} != "/" ]]; then
-		REDIRECT_TARGET="$REDIRECT_TARGET/"
+		REDIRECT_TARGET="${REDIRECT_TARGET}/"
 	fi
 fi
 
 # Default to 80
 LISTEN="80"
-# Listen to PORT variable given on Cloud Run Context
-if [ ! -z "$PORT" ]; then
-	LISTEN="$PORT"
+# VIRTUAL_PORT is a variable defined in nginxproxy/nginx-proxy docker image
+if [ -n "${VIRTUAL_PORT}" ]; then
+	LISTEN="${VIRTUAL_PORT}"
 fi
 
 cat <<EOF > /etc/nginx/conf.d/default.conf
 server {
 	listen ${LISTEN};
-
-	rewrite ^/(.*)\$ ${REDIRECT_TARGET}\$1 ${REDIRECT_TYPE};
+	return ${REDIRECT_CODE} ${REDIRECT_TARGET};
 }
 EOF
 
-
-echo "Listening to $LISTEN, Redirecting HTTP requests to ${REDIRECT_TARGET}..."
+echo "Listening to ${LISTEN}, Redirecting HTTP requests to ${REDIRECT_TARGET} (${REDIRECT_CODE})"
 
 exec nginx -g "daemon off;"
